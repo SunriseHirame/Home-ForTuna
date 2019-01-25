e52f9956c9e11ef9b9e4bd1a73b3b7ef17869528
Ibel44 <37579193+Ibel44@users.noreply.github.com>
2019-01-25 22:56:43 +0200

    First draft of basket collision detection (#2)

---
+++ Assets/Iiro/CollisionDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public Text TextObject;
+    public int Score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag (gameObject.tag))
+        {
+            Score += 20;
+            TextObject.text = "Score: " + Score.ToString();
+        }
+        else
+        {
+            Score += 10;
+            TextObject.text = "Score: " + Score.ToString();
+        }
+    }
+}
