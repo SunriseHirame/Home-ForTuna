11a2bc86e68635a790b22f1251528f3a589ad8b1
JereE <jere.eklund@gmail.com>
2019-01-25 22:23:34 +0200

    f

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FishController : MonoBehaviour {
+    public Rigidbody Mid;
+
+    public float Force = 1;
+
+    private bool _isBounced = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) > 20) {
+            Destroy(this.gameObject);
+        }
+        transform.Translate(0.1f,0,0,Space.Self);
+        if (Random.value < 0.01f && !_isBounced) {
+            Bounce();
+        }
+    }
+
+    private void Bounce() {
+        foreach (Rigidbody rb in transform.GetComponentsInChildren<Rigidbody>()) {
+            rb.isKinematic = false;
+        }
+        Vector3 Force = new Vector3(Random.Range(-1,1),Random.Range(0.5f,1),0);
+        Mid.AddForce(Force.normalized*this.Force,ForceMode.Impulse);
+        _isBounced = true;
+    }
+}
---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour {
+    
+    public float SpawnDelay = 3;
+    public GameObject[] Fish = new GameObject[0];
+    private float _timer = 0;
+    
+    private void Update() {
+        _timer += Time.deltaTime;
+        if (_timer >= SpawnDelay) {
+            _timer = 0;
+            SpawnFish();
+        }
+    }
+
+    private void SpawnFish() {
+        GameObject f = Fish[Random.Range(0, Fish.Length)];
+        Vector3 pos = Vector3.right * (Random.value < 0.5f ? -12 : 12);
+        Quaternion rot = Quaternion.identity;
+        if (pos.x > 0) {
+            rot = Quaternion.Euler(0,0,180);
+        }
+        Instantiate(f, pos, rot);
+        
+    }
+}
