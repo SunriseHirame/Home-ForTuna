141b05e9c0534b90fdf7cc616797577cfd016c3f
JereE <jere.eklund@gmail.com>
2019-01-26 14:33:23 +0200

    asd

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public int CurrentScore = 0;
+    public Text TextObject;
+
+    private void Update() {
+        TextObject.text = "Score: " + CurrentScore.ToString();
+    }
+}
