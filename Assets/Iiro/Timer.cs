e4084e8a14737988cc1856f3c61df50341cf9e11
Ibel44 <37579193+Ibel44@users.noreply.github.com>
2019-01-27 01:18:13 +0200

    Iiro game session timer + rudimentary game over screen (#17)
    * asd
    
    * Timer + rudimentary game over screen
    
    Game ends after x seconds and shows end score for x seconds. Then reloads active scene.

---
+++ Assets/Iiro/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject MainMenuCanvas;
+    public GameObject GameOverCanvas;
+    public Score PlayerScore;
+    public TMP_Text EndScoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuCanvas.gameObject.SetActive (false);
+        GameOverCanvas.gameObject.SetActive (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad >= 20)
+        {
+            EndScoreText.text = "Score: " + PlayerScore.CurrentScore.ToString ();
+            GameOverCanvas.gameObject.SetActive (true);
+            Invoke (nameof(hardRestartGame), 5);
+        }
+    }
+    
+    void hardRestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
