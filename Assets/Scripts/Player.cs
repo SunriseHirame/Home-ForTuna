d5a8b34e040c7e5a0eb83493f67485eea0739549
JereE <jere.eklund@gmail.com>
2019-01-26 14:06:31 +0200

    uu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour {
     public float MovementSpeed = 0.1f;
     public float Limit = 12;
     public float PlayerSize = 2;
     public float BouncyForce = 40;
+    public bool Player2;
+    public UnityEvent Splat;
     void Update()
     {
-        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.x < Limit) {
-            transform.Translate(MovementSpeed,0,0);
+        if (!Player2) {
+            if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < Limit) {
+                transform.Translate(MovementSpeed, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -Limit) {
+                transform.Translate(-MovementSpeed, 0, 0);
+            }
         }
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.x > -Limit) {
-            transform.Translate(-MovementSpeed,0,0);
+        else {
+            if (Input.GetKey(KeyCode.D) && transform.position.x < Limit) {
+                transform.Translate(MovementSpeed, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A) && transform.position.x > -Limit) {
+                transform.Translate(-MovementSpeed, 0, 0);
+            }
         }
     }
 
@@ -24,6 +39,7 @@ public class Player : MonoBehaviour {
         float collisionDeltaX = other.GetContact(0).point.x-transform.position.x;
         float normalized = collisionDeltaX / PlayerSize;
         orb.AddForce(new Vector2(normalized,0.5f).normalized*BouncyForce,ForceMode2D.Impulse);
+        Splat.Invoke();
 
     }
 }
