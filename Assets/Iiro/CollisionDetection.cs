0f306ee4a655927823b1e222da5a3a7cbb968017
Ibel44 <37579193+Ibel44@users.noreply.github.com>
2019-01-26 17:51:59 +0200

    Switched standard UI Text elements to use TextMeshPro (#11)

--- Assets/Iiro/CollisionDetection.cs
+++ Assets/Iiro/CollisionDetection.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CollisionDetection : MonoBehaviour
 {
     
     public Score PlayerScore;
-    public Text PopUpText;
+    public TMP_Text PopUpText;
     
     private void OnTriggerEnter2D (Collider2D other)
     {
@@ -16,14 +17,14 @@ public class CollisionDetection : MonoBehaviour
             int points_correct = 20;
             PlayerScore.CurrentScore += points_correct;
             PopUpText.text = points_correct.ToString ();
-            PopUpText.gameObject.SetActive (true);    // This gets disabled after delay by another script called DisableAfterDelay
+            PopUpText.gameObject.SetActive (true);    // This gets disabled after certain delay by another script called DisableAfterDelay
         }
         else if(!other.gameObject.CompareTag("NotFish"))
         {
             int points_incorrect = 10;
             PlayerScore.CurrentScore += points_incorrect;
             PopUpText.text = points_incorrect.ToString ();
-            PopUpText.gameObject.SetActive (true);    // This gets disabled after delay by another script called DisableAfterDelay
+            PopUpText.gameObject.SetActive (true);    // This gets disabled after certain delay by another script called DisableAfterDelay
         }
     }
 }
