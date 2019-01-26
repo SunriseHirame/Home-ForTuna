57855738bd67bb228c536318bf7ff35d48daf9dc
JereE <jere.eklund@gmail.com>
2019-01-26 13:41:31 +0200

    jotain

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,17 @@ using Unity.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float MovementSpeed = 0.1f;
     public float Limit = 12;
     public float PlayerSize = 2;
     public float BouncyForce = 40;
     void Update()
     {
         if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.x < Limit) {
-            transform.Translate(0.1f,0,0);
+            transform.Translate(MovementSpeed,0,0);
         }
         if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.x > -Limit) {
-            transform.Translate(-0.1f,0,0);
+            transform.Translate(-MovementSpeed,0,0);
         }
     }
 
@@ -22,7 +23,7 @@ public class Player : MonoBehaviour {
         orb.velocity = Vector2.zero;
         float collisionDeltaX = other.GetContact(0).point.x-transform.position.x;
         float normalized = collisionDeltaX / PlayerSize;
-        orb.AddForce(new Vector2(normalized,1).normalized*BouncyForce,ForceMode2D.Impulse);
+        orb.AddForce(new Vector2(normalized,0.5f).normalized*BouncyForce,ForceMode2D.Impulse);
 
     }
 }
