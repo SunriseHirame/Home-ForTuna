08e7d0c2700777c43268cc488a09a458a93f5020
Azethzotyoce <jere.eklund@gmail.com>
2019-01-26 16:12:02 +0200

    ääniä yms (#9)

---
+++ Assets/Scripts/RandomScale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        float scale = Random.Range(0.5f, 1.5f);
+        transform.localScale = transform.localScale * scale;
+    }
+}
---
+++ Assets/Scripts/Splasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Splasher : MonoBehaviour {
+    private bool _firstSplash;
+    public UnityEvent Splash;
+
+    private void OnTriggerExit(Collider other) {
+        if (!_firstSplash && other.gameObject.tag.Equals("Water")) {
+            transform.rotation = Quaternion.Euler(-90,0,0);
+            Splash.Invoke();
+            _firstSplash = true;
+        }
+    }
+}
