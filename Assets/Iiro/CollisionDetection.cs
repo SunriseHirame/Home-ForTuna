1f602494c411f4f7cd1ceae06e96528f18c9149a
Azethzotyoce <jere.eklund@gmail.com>
2019-01-26 14:33:50 +0200

    Jere jotain (#7)
    * jotain
    
    * uu
    
    * asd

--- Assets/Iiro/CollisionDetection.cs
+++ Assets/Iiro/CollisionDetection.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class CollisionDetection : MonoBehaviour
 {
-    public Text ScoreText;
-    public Text PopUpText;
-    public int Score = 0;
     
+    public Score PlayerScore;
     
-    private void OnTriggerEnter (Collider other)
+    private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.CompareTag (gameObject.tag))
         {
-            Score += 20;
-            ScoreText.text = "Score: " + Score.ToString();
+            PlayerScore.CurrentScore += 20;
         }
         else
         {
-            Score += 10;
-            ScoreText.text = "Score: " + Score.ToString();
+            if(!other.gameObject.CompareTag("NotFish"))
+                PlayerScore.CurrentScore += 10;
         }
-        
-        PopUpText.gameObject.SetActive (true);    // PopUpTextTest gets deactivated after delay by its own script
-        
     }
 }
