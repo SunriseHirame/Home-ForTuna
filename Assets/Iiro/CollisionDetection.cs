160446d1ae6ef8b09bb7d40da60ec6e4fbfaa37f
Ibel44 <37579193+Ibel44@users.noreply.github.com>
2019-01-26 15:37:50 +0200

    Pop-up text after getting points (#8)

--- Assets/Iiro/CollisionDetection.cs
+++ Assets/Iiro/CollisionDetection.cs
@@ -7,17 +7,24 @@ public class CollisionDetection : MonoBehaviour
 {
     
     public Score PlayerScore;
+    public Text PopUpText;
     
     private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.CompareTag (gameObject.tag))
         {
-            PlayerScore.CurrentScore += 20;
+            int points_correct = 20;
+            PlayerScore.CurrentScore += points_correct;
+            PopUpText.text = points_correct.ToString ();
+            PopUpText.gameObject.SetActive (true);    // This gets disabled after delay by another script called DisableAfterDelay
         }
         else
         {
+            int points_incorrect = 10;
             if(!other.gameObject.CompareTag("NotFish"))
-                PlayerScore.CurrentScore += 10;
+                PlayerScore.CurrentScore += points_incorrect;
+            PopUpText.text = points_incorrect.ToString ();
+            PopUpText.gameObject.SetActive (true);    // This gets disabled after delay by another script called DisableAfterDelay
         }
     }
 }
