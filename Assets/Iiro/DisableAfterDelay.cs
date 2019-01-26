4069dbbba28bd39e3c00e7621f08027a5b3f9468
Iiro Kiema <37579193+Ibel44@users.noreply.github.com>
2019-01-26 13:21:53 +0200

    Basket collision pop-up score
    Basket collision now shows rudimentary pop up of the given score

---
+++ Assets/Iiro/DisableAfterDelay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAfterDelay : MonoBehaviour
+{
+    public float Delay;
+
+    private void OnEnable ()
+    {
+        Invoke (nameof(Disable), Delay);
+    }
+
+    private void Disable ()
+    {
+        gameObject.SetActive (false);
+    }
+}
