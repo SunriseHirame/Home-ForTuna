a0330d407c7321a590a6473d10055606d747aedd
JereE <jere.eklund@gmail.com>
2019-01-27 14:10:16 +0200

    afs

--- Assets/Scripts/ROllingPropellers.cs
+++ Assets/Scripts/ROllingPropellers.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ROllingPropellers : MonoBehaviour {
     public GameObject leftprop;
     public GameObject rightprop;
+    public UnityEvent left;
+    public UnityEvent right;
     void Update()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
             leftprop.transform.Rotate(Vector3.right,10);
+            left.Invoke();
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
             rightprop.transform.Rotate(Vector3.right,10);
+            right.Invoke();
         }
     }
 }
