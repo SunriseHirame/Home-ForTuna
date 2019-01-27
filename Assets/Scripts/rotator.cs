ea2f94f2ad237fca7543a01bd8e339a3dc7fc6f3
Azethzotyoce <jere.eklund@gmail.com>
2019-01-27 15:10:09 +0200

    asd (#22)

---
+++ Assets/Scripts/rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+    public Vector3 axis;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position,axis,10);
+    }
+}
