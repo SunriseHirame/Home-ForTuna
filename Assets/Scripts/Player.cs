d1efce2a87e0a5250550e2b98be5d0e34b5505f4
Azethzotyoce <jere.eklund@gmail.com>
2019-01-27 14:12:55 +0200

    Jere jotain (#20)
    * 343434
    
    * qwe
    
    * asd
    
    * afs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,27 +9,15 @@ public class Player : MonoBehaviour {
     public float Limit = 12;
     public float PlayerSize = 2;
     public float BouncyForce = 40;
-    public bool Player2;
     public UnityEvent Splat;
-    void Update()
-    {
-        if (!Player2) {
-            if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < Limit) {
-                transform.Translate(MovementSpeed, 0, 0);
-            }
 
-            if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -Limit) {
-                transform.Translate(-MovementSpeed, 0, 0);
-            }
+    void Update() {
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.x < Limit) {
+            transform.Translate(MovementSpeed, 0, 0);
         }
-        else {
-            if (Input.GetKey(KeyCode.D) && transform.position.x < Limit) {
-                transform.Translate(MovementSpeed, 0, 0);
-            }
 
-            if (Input.GetKey(KeyCode.A) && transform.position.x > -Limit) {
-                transform.Translate(-MovementSpeed, 0, 0);
-            }
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.x > -Limit) {
+            transform.Translate(-MovementSpeed, 0, 0);
         }
     }
 
@@ -44,12 +32,12 @@ public class Player : MonoBehaviour {
                 rb.velocity = Vector2.zero;
             }
         }
+
         Rigidbody2D orb = other.rigidbody;
         orb.velocity = Vector2.zero;
-        float collisionDeltaX = other.GetContact(0).point.x-transform.position.x;
+        float collisionDeltaX = other.GetContact(0).point.x - transform.position.x;
         float normalized = collisionDeltaX / PlayerSize;
-        orb.AddForce(new Vector2(normalized,0.5f).normalized*BouncyForce,ForceMode2D.Impulse);
+        orb.AddForce(new Vector2(normalized, 0.5f).normalized * BouncyForce, ForceMode2D.Impulse);
         Splat.Invoke();
-
     }
 }
